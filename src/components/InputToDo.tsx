8ad3021671bd172f5b2718e57c80329ebd86dd7b
Sanganu <kppkmsanu@gmail.com>
2025-08-13 13:15:32 -0600

    avoid React input which is not used

--- src/components/InputToDo.tsx
+++ src/components/InputToDo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FormEvent ,useRef} from 'react';
+import  { useState, type FormEvent ,useRef} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { type InputToDoProps } from '../resources/types/propsTypes';
