9aadda49560d949f91c9bb20d7105607beba4d3b
Sanganu <kppkmsanu@gmail.com>
2025-09-01 18:44:15 -0600

    Adding todo list with modal

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,32 +1,98 @@
+import { useState } from "react";
 import { type ToDoProps, type Todo } from "../resources/types/propsTypes";
 import Card from 'react-bootstrap/Card';
+import { Button, Modal, Form } from 'react-bootstrap';
 
-const Goal = ({ title, description, priority, dueDate, status, id,  onDelete, onUpdateGoal }: ToDoProps) => {
+const Goal = ({ title, description, priority, dueDate, status, id, onDelete, onUpdateGoal }: ToDoProps) => {
+    const [isEditing, setIsEditing] = useState(false)
+
+
     return (<>
-       <Card
-        //   bg='info'
-        //   text= 'white'
-          className="mb-2 goal-card"
-          border="warning"
-          data-id={id}>
-           <Card.Header>
-                {title}
+
+        <Card
+            bg={status === "todo" ? "info" : status === "in-progress" ? "warning" : "success"}
+            className="mb-2 goal-card"
+            border="warning"
+            data-id={id}>
+            <Card.Header>
+                   <h6>{priority}</h6>
+                <p>{status.toLocaleUpperCase()}</p>
+               
             </Card.Header>
-             <Card.Body>
+            <Card.Body>
+                <h4>{title}</h4>
                 <p>{description}</p>
 
-            <p>{(dueDate instanceof Date) ? dueDate.toLocaleDateString() : dueDate}</p>
-            <p>{priority}</p>
-            <p>{status}</p>
-             </Card.Body>
+                <p>{(dueDate instanceof Date) ? dueDate.toLocaleDateString() : dueDate}</p>
+                <p>{priority}</p>
+                <p>{status}</p>
+            </Card.Body>
             <Card.Footer>
-            <button className="btn btn-warning m-2"
-             onClick={() => onUpdateGoal({title,description,status,priority,dueDate,id} as Todo)}>Edit</button>
-            <button className="btn btn-danger m-2" 
-            onClick={() => onDelete(id)}>Delete</button>
+                <button className="btn btn-warning m-2"
+                    onClick={() => setIsEditing(prevEditing => !prevEditing)}>Edit</button>
+                <button className="btn btn-danger m-2"
+                    onClick={() => onDelete(id)}>Delete</button>
             </Card.Footer>
         </Card>
-    </>)
+
+        <Modal show={isEditing} onHide={() => setIsEditing(prevEditing => !prevEditing)}>
+            <Modal.Header closeButton>
+                <Modal.Title>Edit Goal/Task</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form className='container' onSubmit={() => onUpdateGoal({ title, description, status, priority, dueDate, id } as Todo)}>
+                    <Form.Group className="mb-3" controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Title" value={title} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" placeholder="Description" value={description} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="priority">
+                        <Form.Label>Priority</Form.Label>
+                        <Form.Select aria-label="Default select" value={priority} defaultValue="2">
+                            <option>Open this select menu</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
+                        </Form.Select>
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="status">
+                        <Form.Label>Status</Form.Label>
+                        <Form.Select aria-label="Default select" value={status} defaultValue="1">
+                            <option>Open this select menu</option>
+                            <option value="todo">ToDo</option>
+                            <option value="in-progress">In Progress</option>
+                            <option value="done">Done</option>
+                        </Form.Select>
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="dueDate">
+                        <Form.Label>Due Date</Form.Label>
+                        <Form.Control type="date" value={( dueDate instanceof Date) ? dueDate.toISOString().split('T')[0] : dueDate} />
+                    </Form.Group>
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" size="lg" type="submit">
+                            Update Goal/Task
+                        </Button>
+                    </div>
+                </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => setIsEditing(prevEditing => !prevEditing)}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={() => onUpdateGoal({ title, description, status, priority, dueDate, id } as Todo)}>
+                    Save changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+    )
 }
 
 export default Goal;  
