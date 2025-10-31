125a59a68153ede522dde2c3785688225260d3e7
Sanganu <kppkmsanu@gmail.com>
2025-10-31 07:32:10 -0600

    cyp config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
