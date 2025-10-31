3ceff5c522af9a74c005786a6402f7c8d94b078a
Sanganu <kppkmsanu@gmail.com>
2025-10-31 12:09:29 -0600

    Component.ts

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 // cypress/support/component.ts
 import './commands';   // import custom Cypress commands
 import { mount } from 'cypress/react';
-/* eslint-disable @typescript-eslint/no-namespace */
-//add TypeScript declaration for custom 'mount' command
+
+// add TypeScript declaration for custom 'mount' command
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -11,17 +12,6 @@ declare global {
   }
 }
 
-/*
-declare module 'cypress'{
-  interface Chainable{
-    mount: typeof mount;
-  }
-}
-This option of module augmentation fix doesn't work,
-Cypress exports a global namespace, not a module. TypeScript does not allow augmentation of an entity that is not a module, resulting in the error:
-
-Cannot augment module 'cypress' because it resolves to a non-module entity
-*/
 Cypress.Commands.add('mount', mount);
 
 // Optionally, set global hooks
