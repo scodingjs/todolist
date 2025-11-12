c1cf3d2b52aa5b0a677bb5663d3b800640d63e0f
Sanganu <kppkmsanu@gmail.com>
2025-11-12 07:16:38 -0700

    Adding todo

--- src/components/AllToDos.tsx
+++ src/components/AllToDos.tsx
@@ -7,8 +7,9 @@ import Row from "react-bootstrap/Row"
 
 const AllGoals = ({todos,onDeleteGoal,onUpdateGoal}:AllToDosProps) => {
    
-    return( <Container className="my-4 p-4 m-5">
-         <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+    return( <Container className="my-4 p-4 m-5" data-testId="todos-container">
+        {todos.length === 0 ? <h4 data-testId ="empty-todos">No Todo's yet. Add your first goal!</h4>:
+         <Row data-testId="todos-list" xs={1} sm={2} md={3} lg={4} className="g-4">
         {todos.map((goal,key) => (<ToDo
             {...goal}
             key = {key} 
@@ -16,7 +17,7 @@ const AllGoals = ({todos,onDeleteGoal,onUpdateGoal}:AllToDosProps) => {
             onUpdateGoal={onUpdateGoal}
             />
         ))}
-        </Row>
+        </Row>}
         </Container>)
 }
 
