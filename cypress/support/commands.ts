4144cb886bdbd1d246e633511c76858f2f3c5099
Sanganu <kppkmsanu@gmail.com>
2025-11-18 10:02:56 -0700

    Move declare namespace to index.d.ts

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -9,24 +9,6 @@
 // https://on.cypress.io/custom-commands
 
 import { type Todo } from "../../src/resources/types/propsTypes";
-// Extend Cypress' Chainable interface for TypeScript autocomplete
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Verifies that a form field has a label, is visible,
-       * and has the expected id attribute.
-       * @param labelFor - the "for" attribute of the <label>
-       * @param testId - the data-testid value for the input/select
-       * @param idAttr - the expected id value for the field
-       */
-      verifyField(labelFor: string, testId: string, idAttr: string): Chainable<void>;
-      getTestById(testId: string): Chainable<JQuery<HTMLElement>>;
-      clearAllTodos(): Chainable<void>;
-      addTodo(todo: Todo): Chainable<void>
-    }
-  }
-}
 
 // Implementation of VerifyField
 Cypress.Commands.add('verifyField', (labelFor, testId, idAttr) => {
